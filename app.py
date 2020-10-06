b50cf4ab6feb55e76212a122cd1af3640cab4de8
Brian Schaefer <physinet@gmail.com>
2020-10-06 16:40:28 -0400

    Display data from a dataframe or dictionary
    Next steps:
    - make pretty table
    - Get real data in the table

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 from flask import Flask, render_template
+import pandas as pd
+
+rows = [
+    {'a': 1, 'b': 2, 'c':3},
+    {'a': 3, 'b': 5, 'c':-6}
+]
+
+df = pd.DataFrame(rows)
 
 def create_app(configfile=None):
     app = Flask(__name__)
@@ -7,7 +15,16 @@ def create_app(configfile=None):
     def index():
         return render_template('index.html')
 
+    @app.route('/table')
+    def table():
+        return render_template('table.html', rows=rows)
+
+    @app.route('/df')
+    def render_df():
+        return render_template('df.html', df=df)
+
     return app
+
 
 
 # if __name__ == '__main__':
