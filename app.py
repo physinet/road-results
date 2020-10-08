6cfd7c10c241735b1f67ba9460e303b4eb8dcb7a
Brian Schaefer <physinet@gmail.com>
2020-10-08 12:15:53 -0400

    Display race data in table
    Sample race for now

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template
 import pandas as pd
+import dill
 
 rows = [
     {'a': 1, 'b': 2, 'c':3},
     {'a': 3, 'b': 5, 'c':-6}
-]
+] * 17
 
 df = pd.DataFrame(rows)
+
+file = r'C:\data\results\races\1000.pkd'
+json = dill.load(open(file, 'rb'))
+df_race = pd.read_json(json)
+columns = [str(i) for i in range(28)]
+df_race = df_race.drop(columns=columns)
+
+df = df_race[['FirstName', 'LastName', 'TeamName', 'RaceTime']]
 
 def create_app(configfile=None):
     app = Flask(__name__)
@@ -17,7 +26,7 @@ def create_app(configfile=None):
 
     @app.route('/table')
     def table():
-        return render_template('table.html', rows=rows)
+        return render_template('table.html', df=df)
 
     @app.route('/df')
     def render_df():
