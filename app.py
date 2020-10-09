512c956886c2bf42aaaba3d18f937be7f36d671f
Brian Schaefer <physinet@gmail.com>
2020-10-09 14:47:53 -0400

    Search box with suggestions
    Can get submitted info

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from flask_wtf import FlaskForm
+from wtforms import SelectField, validators
 import pandas as pd
 import dill
 
@@ -15,18 +17,45 @@ df_race = pd.read_json(json)
 columns = [str(i) for i in range(28)]
 df_race = df_race.drop(columns=columns)
 
+df_race = df_race[df_race['RaceCategoryName'] == 'Category 3']
 df = df_race[['FirstName', 'LastName', 'TeamName', 'RaceTime']]
+
+race_names = ['test1', 'test2', 'test3', 'bucknell']
+possible_names = {'0': 'hans', '1': 'sepp', '3': 'max'}
+
+
+class RaceForm(FlaskForm):
+    name = SelectField('race_name',
+                            # choices=race_names,
+                            choices=[("", "")] + [(uuid, name) for uuid, name in possible_names.items()],  # [("", "")] is needed for a placeholder
+                            validators=[validators.InputRequired()])
 
 def create_app(configfile=None):
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'YOUR SECRET KEY'
 
     @app.route('/')
     def index():
-        return render_template('index.html')
+        return render_template('index.html', race_list=race_names,
+                                             form=RaceForm(),
+                                             race_name='Test race name')
 
     @app.route('/table')
     def table():
         return render_template('table.html', df=df)
+
+    @app.route('/', methods=['POST'])
+    def index_post():
+        race_name = request.form['race_name']
+        return render_template('index.html', race_list=race_names,
+                                             form=RaceForm(),
+                                             scroll='results',
+                                             race_name=race_name)
+    @app.route("/search/<string:box>")
+    def process(box):
+        query = request.args.get('query')
+        suggestions = [{'value': 'joe','data': 'joe'}, {'value': 'jim','data': 'jim'}]
+        return jsonify({"suggestions":suggestions})
 
     @app.route('/df')
     def render_df():
