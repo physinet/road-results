d068071d19fd8f7c7fe11142f6a290fa7c28b05d
Brian Schaefer <physinet@gmail.com>
2020-10-26 16:02:43 -0400

    Add tooltip on altair plot

--- plotting.py
+++ plotting.py
@@ -9,9 +9,12 @@ import altair as alt
 
 def make_racer_plot_alt(df_racer):
     """Plot each racer's rating over time using altair"""
-    chart = alt.Chart(df_racer.reset_index()).mark_line().encode(
-        x="index",
-        y="Points"
+    chart = alt.Chart(df_racer.reset_index()).encode(
+        x='index',
+        y='Points',
+        tooltip=['index', 'Points']
     )
+
+    chart = chart.mark_line() + chart.mark_point()
 
     return chart.to_json()
