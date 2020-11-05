859c9276c27deb4eca0b62b779ecb72edab512bb
Brian Schaefer <physinet@gmail.com>
2020-11-05 12:12:20 -0500

    Improve merge with prior ratings
    Still using dataframes, should consider switching to sqlalchemy

--- ratings.py
+++ ratings.py
@@ -4,29 +4,25 @@ from database import db
 from model import Racers, Results
 
 env = ts.TrueSkill(backend='scipy', draw_probability=0)
-
+default_ratings = {'rating_mu': env.mu, 'rating_sigma': env.sigma}
 
 def _get_ratings(df):
     """Get ratings for a DataFrame of racers ordered by placing"""
-    # Insert results into table with default ratings
-    df['prior_rating_mu'] = env.mu
-    df['prior_rating_sigma'] = env.sigma
-    df['rating_mu'] = env.mu
-    df['rating_sigma'] = env.sigma
 
-    # Get existing ratings from racers table and update prior ratings
+    # Get existing ratings from racers table
+    # This df has columns RacerID, rating_mu, rating_sigma
     existing_ratings = Racers.get_ratings(df['RacerID'])
-    # df = df.merge(existing_ratings, on=['RacerID', ''] how='left')
-    df = df.merge(existing_ratings, on=['RacerID'], how='left')
 
-    df['prior_rating_mu'] = df['rating_mu_y'].fillna(df['rating_mu_x'])
-    df['prior_rating_sigma'] = df['rating_sigma_y'].fillna(df['rating_sigma_x'])
-    df = df.drop(columns=['rating_mu_x', 'rating_mu_y',
-                    'rating_sigma_x', 'rating_sigma_y'])
+    # merge into the results dataframe on RacerID
+    # fill missing values with default ratings
+    df = df.merge(existing_ratings, on=['RacerID'], how='left') \
+           .fillna(default_ratings) \
+           .rename(columns={'rating_mu': 'prior_rating_mu',
+                            'rating_sigma': 'prior_rating_sigma'})
 
     # Calculate new ratings
-    df = df.assign(rating_mu=df['prior_rating_mu'] * 2,
-                   rating_sigma=df['prior_rating_sigma'] ** 4)
+    df = df.assign(rating_mu=df['prior_rating_mu'] + 1,
+                   rating_sigma=df['prior_rating_sigma'] + 1)
 
     # Update racers table with new ratings
     df_for_update = df[df['RacerID'].isin(existing_ratings['RacerID'])]
